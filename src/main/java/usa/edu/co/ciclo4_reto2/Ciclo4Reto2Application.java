aceda76e21db29a59b3747850ce77d2291f52bee
Juan Molano <juan.molano2.mt@correo.usa.edu.co>
2021-12-11 22:14:16 -0500

    segundo commit reto 2

--- src/main/java/usa/edu/co/ciclo4_reto2/Ciclo4Reto2Application.java
+++ src/main/java/usa/edu/co/ciclo4_reto2/Ciclo4Reto2Application.java
@@ -1,13 +1,35 @@
 package usa.edu.co.ciclo4_reto2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import usa.edu.co.ciclo4_reto2.repository.crud.LaptopCrudRepository;
+import usa.edu.co.ciclo4_reto2.repository.crud.UserCrudRepository;
 
+@Component
 @SpringBootApplication
-public class Ciclo4Reto2Application {
+public class Ciclo4Reto2Application implements CommandLineRunner {
+	@Autowired
+	private UserCrudRepository userCrudRepository;
+
+	@Autowired
+	private LaptopCrudRepository laptopCrudRepository;
+
+	/*@Autowired
+	private OrderCrudRepository orderCrudRepository;*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo4Reto2Application.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		userCrudRepository.deleteAll();                                                            //Para que la base de datos se borre por cada ejecución
+		laptopCrudRepository.deleteAll();
+		/*orderCrudRepository.deleteAll();*/
+	}
+
 
 }
